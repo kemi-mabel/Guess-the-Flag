0e5c583b3c426966b0819cf5ad0b9dc60959f11c
Oluwakemi Onajinrin <oluwakemi.mabell@gmail.com>
2022-04-05 13:03:34 +0100

    Initial Commit

---
+++ Guess the Flag/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Oluwakemi Onajinrin on 4/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    
+    @State private var scoreTitle = ""
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack{
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+    //                        .renderingMode(.original)
+                    }
+                }
+            }
+        }
+
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+//    func askQuestion() {
+//        countries.shuffle()
+//        correctAnswer = Int.random(in: 0...2)
+//    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Oluwakemi Onajinrin on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
